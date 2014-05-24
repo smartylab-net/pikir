cfff1d73cbad7391e81b75d38ce51bfd178b6854
Adilet <adilet.a93@gmail.com>
2014-05-24 19:01:09 +0600

    header  unfixed

--- src/Info/ComplaintBundle/Resources/public/js/jquery_rating.js
+++ src/Info/ComplaintBundle/Resources/public/js/jquery_rating.js
@@ -1,28 +1,7 @@
 $(function(){
 
     var availableTags = [
-        "ActionScript",
-        "AppleScript",
-        "Asp",
-        "BASIC",
-        "C",
-        "C++",
-        "Clojure",
-        "COBOL",
-        "ColdFusion",
-        "Erlang",
-        "Fortran",
-        "Groovy",
-        "Haskell",
-        "Java",
-        "JavaScript",
-        "Lisp",
-        "Perl",
-        "PHP",
-        "Python",
-        "Ruby",
-        "Scala",
-        "Scheme"
+
     ];
     $( "#Complaint_company" ).autocomplete({
         source: availableTags

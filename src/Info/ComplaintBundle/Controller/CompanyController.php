416f968228b28b9769da2b5822ee283af3640b8e
nakusyat <nakusyat@inbox.ru>
2014-05-24 21:14:02 +0600

    before push into master 21:13

--- src/Info/ComplaintBundle/Controller/CompanyController.php
+++ src/Info/ComplaintBundle/Controller/CompanyController.php
@@ -8,14 +8,24 @@ class CompanyController extends Controller
 {
     public function indexAction($id)
     {
-    	$company = $this->getDoctrine()->getRepository('InfoComplaintBundle:Company')->find($id);
-
+    	$companyRepository = $this->getDoctrine()->getRepository('InfoComplaintBundle:Company');
+    	$complaintRepository = $this->getDoctrine()->getRepository('InfoComplaintBundle:Complaint');
+    	
+    	$company = $companyRepository->find($id);
     	if(!$company)
     	{
     		throw $this->createNotFoundException('The company does not exist');
     	}
+		
+		$complaintList = $complaintRepository->findByCompany($id);
+    	if(!$complaintList)
+    	{
+    		throw $this->createNotFoundException('The complaintList does not exist');
+    	}
 
-        return $this->render('InfoComplaintBundle:Company:companyPage.html.twig', array('name' => $company));
+        $average = $companyRepository->getComplaintsAverageRating($id);
+      
+        return $this->render('InfoComplaintBundle:Company:companyPage.html.twig', array('company' => $company,'complaintlist'=>$complaintList, 'average'=>$average[0][1]));
 
     }
 }

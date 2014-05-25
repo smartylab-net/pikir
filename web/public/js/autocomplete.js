48dee77db87a4ef27beb1569f1e18fb1d3eb96e3
Nurlanbek Moldomurov <bupychuk1989@gmail.com>
2014-05-26 02:03:00 +0600

    autocomplete label

--- web/public/js/autocomplete.js
+++ web/public/js/autocomplete.js
@@ -2,8 +2,13 @@ $(function() {
     $( "#searching" ).autocomplete({
         source: Routing.generate("search_auto"),
         minLength: 	2,
+        focus: function( event, ui ) {
+            $( "#searching" ).val( ui.item.label );
+            return false;
+        },
         select: function( event, ui ) {
                 window.location = Routing.generate(ui.item.route,{"id":ui.item.value})
+            return false;
 
         }
     });

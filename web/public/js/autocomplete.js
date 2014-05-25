459495f67d6c9a2f3b3d49564fb2eb9868866585
Nurlanbek Moldomurov <bupychuk1989@gmail.com>
2014-05-26 00:26:11 +0600

    autocomplete

--- web/public/js/autocomplete.js
+++ web/public/js/autocomplete.js
@@ -1,6 +1,6 @@
 $(function() {
     $( "#searching" ).autocomplete({
-        source: "http://localhost/hackaton/web/app_dev.php/autocomplete/",
+        source: Routing.generate("search_auto"),
         minLength: 	2,
         select: function( event, ui ) {
                 window.location = Routing.generate(ui.item.route,{"id":ui.item.value})

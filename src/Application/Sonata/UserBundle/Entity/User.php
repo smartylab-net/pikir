c9bab423e7a59b02520adbf5f5fe6b6b185cee2d
Nurlanbek Moldomurov <bupychuk1989@gmail.com>
2014-05-26 21:05:12 +0600

    Вывод имени пользователя...

--- src/Application/Sonata/UserBundle/Entity/User.php
+++ src/Application/Sonata/UserBundle/Entity/User.php
@@ -62,7 +62,7 @@ class User extends BaseUser
 
     public function __toString()
     {
-        return $this->getFullname()? :parent::__toString();
+        return trim($this->getFullname())!=''? $this->getFullname():parent::__toString();
     }
 
 }

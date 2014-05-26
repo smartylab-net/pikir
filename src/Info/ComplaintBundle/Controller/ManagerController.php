6be326de6b65de690708e0e80e855034c166f521
Nurlan <bupychuk1989@gmail.com>
2014-05-26 14:12:38 +0600

    error 500

--- src/Info/ComplaintBundle/Controller/ManagerController.php
+++ src/Info/ComplaintBundle/Controller/ManagerController.php
@@ -38,12 +38,12 @@ class ManagerController extends Controller{
                 $em->persist($company);
                 $em->flush();
                 $this->container->get('session')->getFlashBag()->add('manager.company_edit_success', 'Профиль компании обновлен');
-                return $this->redirect($this->generateUrl('info_manager_company_edit',array('id'=>$id)));
+                return $this->redirect($this->generateUrl('info_manager_company_edit',array('company'=>$id)));
             }
             else
             {
                 $this->container->get('session')->getFlashBag()->add('manager.company_edit_error', 'Профиль компании не сохранен, обнаружена ошибка');
-                return $this->redirect($this->generateUrl('info_manager_company_edit',array('id'=>$id)));
+                return $this->redirect($this->generateUrl('info_manager_company_edit',array('company'=>$id)));
             }
         }
 

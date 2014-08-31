b1b12664ffd135e814a4f3320f3f25d062589c9f
Nurlanbek Moldomurov <bupychuk1989@gmail.com>
2014-09-01 00:36:45 +0600

    refactoring и добавление gravatar

---
+++ src/Application/Sonata/UserBundle/Twig/GravatarImageExtension.php
@@ -0,0 +1,36 @@
+<?php
+
+
+namespace Application\Sonata\UserBundle\Twig;
+
+
+use Application\Sonata\UserBundle\Entity\User;
+
+class GravatarImageExtension extends \Twig_Extension
+{
+    public function getFunctions()
+    {
+        return array(
+            new \Twig_SimpleFunction('gravatar_image', array($this, 'gravatarImage'), array('is_safe' => array('html'))),
+        );
+    }
+
+    public function gravatarImage($user, $imageSize, $options = array())
+    {
+        /** @var $user User */
+        if (!($user instanceof User)) {
+            return null;
+        }
+        $hash = md5($user->getEmail());
+        $gravatarLink = "http://www.gravatar.com/avatar/$hash?d=mm&s=$imageSize";
+        $attr = '';
+        foreach ($options as $key => $value)
+            $attr .= $key . '="' . $value . '"';
+        return '<img src="' . $gravatarLink . '" ' . $attr . ' />';
+    }
+
+    public function getName()
+    {
+        return 'gravatar_image_ext';
+    }
+}

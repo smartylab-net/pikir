02ce0d9e1b5d8531d0a722872cefd907bf02dabb
bupychuk <bupychuk1989@gmail.com>
2015-05-24 11:12:53 +0600

    Миграция - удаление поля title

--- app/DoctrineMigrations/Version1.php
+++ app/DoctrineMigrations/Version1.php
@@ -16,6 +16,9 @@ class Version1 extends AbstractMigration
     public function up(Schema $schema)
     {
         // this up() migration is auto-generated, please modify it to your needs
+        $this->abortIf($this->connection->getDatabasePlatform()->getName() != 'mysql', 'Migration can only be executed safely on \'mysql\'.');
+
+        $this->addSql('ALTER TABLE Complaint DROP title');
     }
 
     /**
@@ -24,5 +27,8 @@ class Version1 extends AbstractMigration
     public function down(Schema $schema)
     {
         // this down() migration is auto-generated, please modify it to your needs
+        $this->abortIf($this->connection->getDatabasePlatform()->getName() != 'mysql', 'Migration can only be executed safely on \'mysql\'.');
+
+        $this->addSql('ALTER TABLE Complaint ADD title VARCHAR(255) NOT NULL');
     }
 }

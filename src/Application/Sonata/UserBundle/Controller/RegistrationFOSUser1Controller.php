3b5ddd0ecac7d834c8a1fbd988d5b95a450eb236
Nurlan Moldomurov <bupychuk1989@gmail.com>
2015-05-25 22:22:03 +0600

    HACK-70 Редирект после регистрации

---
+++ src/Application/Sonata/UserBundle/Controller/RegistrationFOSUser1Controller.php
@@ -0,0 +1,79 @@
+<?php
+/*
+ * This file is part of the Sonata package.
+ *
+ * (c) Thomas Rabaix <thomas.rabaix@sonata-project.org>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+
+namespace Application\Sonata\UserBundle\Controller;
+
+use Symfony\Component\HttpFoundation\RedirectResponse;
+use Symfony\Component\HttpKernel\Exception\NotFoundHttpException;
+use FOS\UserBundle\Model\UserInterface;
+use Sonata\UserBundle\Controller\RegistrationFOSUser1Controller as BaseController;
+
+/**
+ * Class SonataRegistrationController
+ *
+ * This class is inspired from the FOS RegistrationController
+ *
+ * @package Sonata\UserBundle\Controller
+ *
+ * @author Hugo Briand <briand@ekino.com>
+ */
+class RegistrationFOSUser1Controller extends BaseController
+{
+    public function registerAction()
+    {
+        $user = $this->container->get('security.context')->getToken()->getUser();
+
+        if ($user instanceof UserInterface) {
+            $this->container->get('session')->getFlashBag()->set('sonata_user_error', 'sonata_user_already_authenticated');
+            $url = $this->container->get('router')->generate('sonata_user_profile_show');
+
+            return new RedirectResponse($url);
+        }
+
+        $form = $this->container->get('sonata.user.registration.form');
+        $formHandler = $this->container->get('sonata.user.registration.form.handler');
+        $confirmationEnabled = $this->container->getParameter('fos_user.registration.confirmation.enabled');
+
+        $process = $formHandler->process($confirmationEnabled);
+        if ($process) {
+            $user = $form->getData();
+
+            $authUser = false;
+            if ($confirmationEnabled) {
+                $this->container->get('session')->set('fos_user_send_confirmation_email/email', $user->getEmail());
+                $route = 'fos_user_registration_check_email';
+            } else {
+                $authUser = true;
+                $route = $this->container->get('session')->get('sonata_basket_delivery_redirect', 'sonata_user_profile_show');
+                $this->container->get('session')->remove('sonata_basket_delivery_redirect');
+            }
+
+            $this->setFlash('fos_user_success', 'registration.flash.user_created');
+            $url = $this->container->get('session')->get('sonata_user_redirect_url');
+
+            if (null === $url || "" === $url) {
+                $url = $this->container->get('router')->generate($route);
+            }
+
+            $response = new RedirectResponse($url);
+
+            if ($authUser) {
+                $this->authenticateUser($user, $response);
+            }
+
+            return $response;
+        }
+
+        return $this->container->get('templating')->renderResponse('FOSUserBundle:Registration:register.html.'.$this->getEngine(), array(
+            'form' => $form->createView(),
+        ));
+    }
+}
---
+++ src/Application/Sonata/UserBundle/Controller/SecurityFOSUser1Controller.php
@@ -0,0 +1,43 @@
+<?php
+/*
+ * This file is part of the Sonata package.
+ *
+ * (c) Thomas Rabaix <thomas.rabaix@sonata-project.org>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+
+namespace Application\Sonata\UserBundle\Controller;
+
+use FOS\UserBundle\Controller\SecurityController;
+use Sonata\UserBundle\Model\UserInterface;
+use Symfony\Component\HttpFoundation\RedirectResponse;
+use Sonata\UserBundle\Controller\SecurityFOSUser1Controller as BaseController;
+
+/**
+ * Class SecurityFOSUser1Controller
+ *
+ * @package Sonata\UserBundle\Controller
+ *
+ * @author Hugo Briand <briand@ekino.com>
+ */
+class SecurityFOSUser1Controller extends BaseController
+{
+    public function loginAction()
+    {
+        $user = $this->container->get('security.context')->getToken()->getUser();
+
+        if ($user instanceof UserInterface) {
+            $this->container->get('session')->getFlashBag()->set('sonata_user_error', 'sonata_user_already_authenticated');
+            $url = $this->container->get('router')->generate('sonata_user_profile_show');
+
+            return new RedirectResponse($url);
+        }
+
+        $this->container->get('session')->set('sonata_user_redirect_url', $this->container->get('request')->headers->get('referer'));
+
+        return parent::loginAction();
+    }
+}

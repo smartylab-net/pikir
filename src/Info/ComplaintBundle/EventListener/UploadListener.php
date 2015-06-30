5326267dda6102fe5f1a7aa4e5eb3164188e122b
Nurlan Moldomurov <bupychuk1989@gmail.com>
2015-07-01 01:36:23 +0600

    HACK-97 Превью картинок при загрузке

--- src/Info/ComplaintBundle/EventListener/UploadListener.php
+++ src/Info/ComplaintBundle/EventListener/UploadListener.php
@@ -2,6 +2,7 @@
 namespace Info\ComplaintBundle\EventListener;
 
 use Application\Sonata\MediaBundle\Entity\Media;
+use Oneup\UploaderBundle\Event\PostUploadEvent;
 use Oneup\UploaderBundle\Uploader\Storage\FilesystemOrphanageStorage;
 use Sonata\MediaBundle\Entity\MediaManager;
 use Symfony\Component\DependencyInjection\ContainerInterface;
@@ -17,25 +18,47 @@ class UploadListener {
         $this->em           = $em;
     }
 
-    public function onUpload(PostPersistEvent $event)
+    public function onUpload(PostUploadEvent $event)
     {
-        $request    = $event->getRequest();
         $response   = $event->getResponse();
         $file       = $event->getFile();
+        $this->container->get('logger')->debug($file->getPathname());
 
-        /** @var FilesystemOrphanageStorage $manager */
-        $manager = $this->container->get('oneup_uploader.orphanage_manager')->get('gallery');
-        $files = iterator_to_array($manager->getFiles());
+        $webFolder = $this->container->getParameter('kernel.root_dir').'/../web';
+        $response['filepath'] = $this->getRelativePath($webFolder, $file->getPathname());
 
-        foreach ($files as $i=>$file) {
+    }
 
+    function getRelativePath($from, $to)
+    {
+        // some compatibility fixes for Windows paths
+        $from = is_dir($from) ? rtrim($from, '\/') . '/' : $from;
+        $to   = is_dir($to)   ? rtrim($to, '\/') . '/'   : $to;
+        $from = str_replace('\\', '/', $from);
+        $to   = str_replace('\\', '/', $to);
+
+        $from     = explode('/', $from);
+        $to       = explode('/', $to);
+        $relPath  = $to;
+
+        foreach($from as $depth => $dir) {
+            // find first non-matching dir
+            if($dir === $to[$depth]) {
+                // ignore this directory
+                array_shift($relPath);
+            } else {
+                // get number of remaining dirs to $from
+                $remaining = count($from) - $depth;
+                if($remaining > 1) {
+                    // add traversals up to first matching dir
+                    $padLength = (count($relPath) + $remaining - 1) * -1;
+                    $relPath = array_pad($relPath, $padLength, '..');
+                    break;
+                } else {
+                    $relPath[0] = '/' . $relPath[0];
+                }
+            }
         }
-
-        /*$response['files'] = array(
-            'name' => $file->getFileName(),
-            'size' => $file->getSize(),
-            'url'  => 'http://hack.home/uploads/gallery/' . $file->getFileName(),
-            'delete_url'   => $delete,
-        );*/
+    return implode('/', $relPath);
     }
 }
